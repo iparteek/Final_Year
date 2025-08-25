8bb8994e86a03fb1f89cef384594979ddf461d05
iparteek <prateek10242@gmail.com>
2025-08-25 20:22:02 +0530

    Test5

--- Final/src/components/InputSection.jsx
+++ Final/src/components/InputSection.jsx
@@ -9,7 +9,7 @@ const InputSection = ({ onGenerate }) => {
     if (!inputValue.trim()) return;
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:5000/generate-face/", {
+      const response = await fetch("https://final-year-29v9.onrender.com/generate-face/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: inputValue }),
